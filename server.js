49b7adb2e7eb0d98cec285a0ad9f00067c768bf3
robinkd6 <rkdcruz18@gmail.com>
2015-12-07 08:53:24 -0800

    added sequelize

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 var express = require('express'),
-	app = express();
+	app = express(), 
+	bodyParser = require('body-parser'),
+	db = require('./models');
 
 
 app.use(express.static(__dirname + '/views'));
-
+app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
 	
 	res.render('index', {name: 'URL shortner'});
 	});
+app.post('/', function(req, res) {
 
+  var newUrl = {
+    id: req.body.id,
+    url: req.body.url,
+    hash: req.body.hash,
+  }
 
 app.listen(3000);
 console.log('Running on the smooth port 3000');
